200f816e5853f4c3e9c13d0d0ad843c84590092a
jersonwei <872937094@qq.com>
2023-03-15 21:35:47 +0800

    ✨ feat(app): session setting

--- app.js
+++ app.js
@@ -5,24 +5,30 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
-
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
 const index = require('./routes/index')
 const users = require('./routes/users')
+const { REDIS_CONF } = require('./conf/db')
 
 // error handler
 onerror(app)
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text']
+  })
+)
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
+app.use(
+  views(__dirname + '/views', {
+    extension: 'ejs'
+  })
+)
 
 // logger
 app.use(async (ctx, next) => {
@@ -32,6 +38,23 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+// session 配置
+app.keys = ['Uisdf_12312#$']
+app.use(
+  session({
+    key: 'weibo.sid', // cookie name 默认是 koa.sid
+    prefix: 'weibo:sess:', // redis key 的前缀 默认是 koa:sess:
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000
+    },
+    // ttl: 24*60*60*1000,
+    store: redisStore({
+      all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+    })
+  })
+)
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
@@ -39,6 +62,6 @@ app.use(users.routes(), users.allowedMethods())
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
-});
+})
 
 module.exports = app
