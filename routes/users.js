36a5b37288c0d5faa69ac0d9c61386a59e1d39f8
jersonwei <872937094@qq.com>
2023-03-16 05:51:46 +0800

    ✨ feat(user): mock login

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,28 @@ router.get('/', function (ctx, next) {
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
-
+// 模拟登陆
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body
+  let userInfo
+  if (userName === 'zhangsan' && password === 'abc') {
+    userInfo = {
+      userId: 1,
+      userName: 'zhangsan',
+      nickName: '张三',
+      gender: 1
+    }
+  }
+  if (userInfo === null) {
+    ctx.body = {
+      errno: 1,
+      msg: '登录失败 '
+    }
+    return
+  }
+  ctx.body = {
+    errno: 0,
+    data: userInfo
+  }
+})
 module.exports = router
