0925aab53465e6c24c2248b50b42c2ad1773a294
jersonwei <872937094@qq.com>
2023-03-19 18:04:00 +0800

    ✨ feat(routes&view): setting page

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@
  * @description 用户路由
  */
 
+const { loginRedirect } = require('../midwares/loginCheck')
+
 const router = require('koa-router')()
 
 /**
@@ -28,5 +30,8 @@ router.get('/login', async (ctx, next) => {
 })
 router.get('/register', async (ctx, next) => {
   await ctx.render('register', getLoginState(ctx))
+})
+router.get('/setting', loginRedirect, async (ctx, next) => {
+  await ctx.render('setting', ctx.session.userInfo)
 })
 module.exports = router
