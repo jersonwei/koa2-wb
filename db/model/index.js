4f66fa1df6425414bcf1bb39c0895679af366480
jersonwei <872937094@qq.com>
2023-03-19 22:05:39 +0800

    ✨ feat(Blog): create blog Model

--- db/model/index.js
+++ db/model/index.js
@@ -2,8 +2,12 @@
  * @description 数据模型入口文件
  */
 
+const Blog = require('./Blog')
 const User = require('./User')
-
+Blog.belongsTo(User, {
+  foreignKey: 'userId'
+})
 module.exports = {
+  Blog,
   User
 }
